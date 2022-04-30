6ba9479a5f4b04f68312ca883a3de080972ae9c6
beaniejoy <lhb1025@gmail.com>
2022-04-30 23:05:28 +0900

    HelloServlet 기본 테스트

--- src/main/java/io/brick/springmvc/basic/HelloServlet.kt
+++ src/main/java/io/brick/springmvc/basic/HelloServlet.kt
@@ -1,4 +1,13 @@
 package io.brick.springmvc.basic
 
-class HelloServlet {
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "helloServlet", urlPatterns = ["/hello"])
+class HelloServlet : HttpServlet() {
+    override fun service(req: HttpServletRequest, res: HttpServletResponse) {
+        println("HelloServlet.service")
+    }
 }
